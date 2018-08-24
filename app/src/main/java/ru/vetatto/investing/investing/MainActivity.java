76b3c75377cb096fda3e43cc95c86c9d70a4b8df
Vetatto <vetatto@mail.ru>
2018-08-24 15:19:08 +0300

    Исправление LogOut

--- app/src/main/java/ru/vetatto/investing/investing/MainActivity.java
+++ app/src/main/java/ru/vetatto/investing/investing/MainActivity.java
@@ -71,11 +71,11 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-       /* DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-        toggle.syncState();*/
+        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -144,5 +144,4 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
 }

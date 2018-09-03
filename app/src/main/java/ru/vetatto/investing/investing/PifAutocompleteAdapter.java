4e8f5d11e6cffbc84aa404d68978eba1a2ca6c95
Vetatto <vetatto@mail.ru>
2018-09-03 22:08:18 +0300

    Pif autocomplete

--- app/src/main/java/ru/vetatto/investing/investing/PifAutocompleteAdapter.java
+++ app/src/main/java/ru/vetatto/investing/investing/PifAutocompleteAdapter.java
@@ -42,53 +42,21 @@ public class PifAutocompleteAdapter extends ArrayAdapter<PifAutocompleteData> {
         }
         return view;
     }
-
     @Override
-    public Filter getFilter() {
-        Filter nameFilter = new Filter() {
-            @Override
-            public CharSequence convertResultToString(Object resultValue) {
-                String str = ((PifAutocompleteData) resultValue).getName();
-                return str;
-            }
+    public View getDropDownView(int position, View convertView,
+                                ViewGroup parent) {
 
-            @Override
-            protected FilterResults performFiltering(CharSequence constraint) {
-                FilterResults filterResults = new FilterResults();
-                if (tempItems == null) {
-                    tempItems = new ArrayList<PifAutocompleteData>(items);
-                }
-                if (constraint == null || constraint.length() == 0) {
-                    ArrayList<PifAutocompleteData> list = new ArrayList<PifAutocompleteData>(tempItems);
-                    filterResults.values = list;
-                    filterResults.count = list.size();
-                } else {
-                    suggestions.clear();
-                    for (PifAutocompleteData people : tempItems) {
-                        if (people.getName().toLowerCase().startsWith(constraint.toString().toLowerCase())) {
-                            suggestions.add(people);
-                        }
-                    }
-                    filterResults.values = suggestions;
-                    filterResults.count = suggestions.size();
-                }
-                return filterResults;
-            }
-
-            @Override
-            protected void publishResults(CharSequence constraint, FilterResults results) {
-                ArrayList<PifAutocompleteData> filter = (ArrayList<PifAutocompleteData>) results.values;
-                if (results != null && results.count > 0) {
-                    clear();
-                    for (PifAutocompleteData cust : filter) {
-                        add(cust);
-                        notifyDataSetChanged();
-                    }
-                } else {
-                    notifyDataSetInvalidated();
-                }
-            }
-        };
-        return nameFilter;
+        View view = convertView;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.uk_autocomplete, parent, false);
+        }
+        PifAutocompleteData people = items.get(position);
+        if (people != null) {
+            TextView lblName = (TextView) view.findViewById(R.id.UkNameLabel);
+            if (lblName != null)
+                lblName.setText(people.getName());
+        }
+        return view;
     }
-}
+    }
